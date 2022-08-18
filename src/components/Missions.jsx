ccb2a36c366081dc8ee18f49fbec62d95498dacf
Teresa Marques <taotaodoce@gmail.com>
2022-08-18 18:27:02 -0300

    requisito 10 concluído

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
-
+        {missions.map((missao) => (
+          <MissionCard
+            name={ missao.name }
+            year={ missao.year }
+            country={ missao.country }
+            destination={ missao.destination }
+            key={ missao.name }
+          />
+        ))}
       </div>
     );
   }
